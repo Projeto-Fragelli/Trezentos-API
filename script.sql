23db4e13618e7e559e9b4f2fb4dc1faaddee27e2
CalebeRios <calebe_rios@hotmail.com>
2018-02-09 12:02:27 -0200

    Modifiyng some tables

--- script.sql
+++ script.sql
@@ -8,7 +8,6 @@ CREATE TABLE IF NOT EXISTS Person (
     personTelephoneDDI varchar(3) NOT NULL,
     personTelephoneDDD varchar(3) NOT NULL,
     personTelephoneNumber varchar(9) NOT NULL,
-
     CONSTRAINT person_pk PRIMARY KEY (idPerson)
 );
 
@@ -28,7 +27,7 @@ CREATE TABLE IF NOT EXISTS Class (
 	classCutOff int NOT NULL,
   classDescription varchar(200) NOT NULL,
   classCreationDate varchar(10) NOT NULL,
-
+	CONSTRAINT Class_Person_fk FOREIGN KEY (idClassCreator) REFERENCES Person (idPerson),
 	CONSTRAINT Class_pk PRIMARY KEY (idClass)
 );
 
@@ -41,62 +40,69 @@ CREATE TABLE IF NOT EXISTS PersonHasClass (
 	CONSTRAINT PersonHasClass_PersonType_fk FOREIGN KEY (idPersonType) REFERENCES PersonType (idPersonType)
 );
 
-CREATE TABLE IF NOT EXISTS ExamType (
-	idExamType int NOT NULL AUTO_INCREMENT,
-	ExamTypeDescription varchar(100) NOT NULL,
-	CONSTRAINT ExamType_pk PRIMARY KEY (idExamType)
-);
-
 CREATE TABLE IF NOT EXISTS Exam (
 	idExam int NOT NULL AUTO_INCREMENT,
 	ExamDescription varchar(15) NOT NULL,
 	idClass int NOT NULL,
-	idExamType int NOT NULL,
 	CONSTRAINT Exam_pk PRIMARY KEY (idExam),
 	CONSTRAINT Exam_Class_fk FOREIGN KEY (idClass) REFERENCES Class (idClass),
-	CONSTRAINT ExamType_Class_fk FOREIGN KEY (idExamType) REFERENCES ExamType (idExamType)
 );
 
 CREATE TABLE IF NOT EXISTS PersonHasExam (
 	idPerson int NOT NULL,
 	idExam int NOT NULL,
+  idClass int NOT NULL,
 	CONSTRAINT PersonHasExam_Person_fk FOREIGN KEY (idPerson) REFERENCES Person (idPerson),
-	CONSTRAINT PersonHasExam_Exam_fk FOREIGN KEY (idExam) REFERENCES Exam (idExam)
+	CONSTRAINT PersonHasExam_Exam_fk FOREIGN KEY (idExam) REFERENCES Exam (idExam),
+    CONSTRAINT PersonHasExam_Class_fk FOREIGN KEY (idClass) REFERENCES Class (idClass)
 );
 
 CREATE TABLE IF NOT EXISTS PersonGroup (
 	idGroup int NOT NULL AUTO_INCREMENT,
-	GroupDescription varchar(45) NOT NULL,
+	groupNumber int NOT NULL,
 	idExam int NOT NULL,
+  idClass int NOT NULL,
 	CONSTRAINT PersonGroup_pk PRIMARY KEY (idGroup),
-	CONSTRAINT PersonGroup_Exam_fk FOREIGN KEY (idExam) REFERENCES Exam (idExam)
+	CONSTRAINT PersonGroup_Exam_fk FOREIGN KEY (idExam) REFERENCES Exam (idExam),
+  CONSTRAINT PersonGroup_Class_fk FOREIGN KEY (idClass) REFERENCES Class (idClass)
 );
 
 CREATE TABLE IF NOT EXISTS PersonHasGroup (
 	idPersonHasGroup int NOT NULL AUTO_INCREMENT,
 	idPerson int NOT NULL,
 	idGroup int NOT NULL,
-	PersonHelper TINYINT(1) NOT NULL,
+	personHelper TINYINT(1) NOT NULL,
 	CONSTRAINT PersonHasGroup_pk PRIMARY KEY (idPersonHasGroup),
 	CONSTRAINT PersonHasGroup_Exam_fk FOREIGN KEY (idPerson) REFERENCES Person (idPerson),
 	CONSTRAINT Group_PersonHasGroup_fk FOREIGN KEY (idGroup) REFERENCES PersonGroup (idGroup)
 );
 
-CREATE TABLE IF NOT EXISTS RateType (
-	idRateType int NOT NULL AUTO_INCREMENT,
-	RateTypeDescription varchar(45) NOT NULL,
-	CONSTRAINT RateType_pk PRIMARY KEY (idRateType)
+CREATE TABLE IF NOT EXISTS Grade (
+  idGrade int NOT NULL AUTO_INCREMENT,
+  grade decimal(10,3) NOT NULL,
+  idPerson int NOT NULL,
+  idExam int NOT NULL,
+  idGradeType int NOT NULL,
+  CONSTRAINT Grade_pk PRIMARY KEY (idGrade),
+	CONSTRAINT Grade_Person_fk FOREIGN KEY (idPerson) REFERENCES Person (idPerson),
+	CONSTRAINT Grade_Exam_fk FOREIGN KEY (idExam) REFERENCES Exam (idExam),
+	CONSTRAINT Grade_GradeType_fk FOREIGN KEY (idGradeType) REFERENCES GradeType (idGradeType)
+);
+
+CREATE TABLE IF NOT EXISTS GradeType (
+  idGradeType int NOT NULL AUTO_INCREMENT,
+  gradeTypeDescription int NOT NULL,
+  CONSTRAINT GradeType_pk PRIMARY KEY (idGradeType)
 );
 
 CREATE TABLE IF NOT EXISTS Rate (
 	idRate int NOT NULL AUTO_INCREMENT,
-	RatesValue int(1) NOT NULL,
-	idRateType int NOT NULL,
+	ratesValue int(1) NOT NULL,
 	idGroup int NOT NULL,
-	idPersonHasGroup int NOT NULL,
+  idPerson int NOT NULL,
+  idPersonEvaluated int NOT NULL,
 	CONSTRAINT Rate_pk PRIMARY KEY (idRate),
-	CONSTRAINT Rate_RateType_fk FOREIGN KEY (idRateType) REFERENCES RateType (idRateType),
-	CONSTRAINT Rate_PersonGroup_fk FOREIGN KEY (idGroup) REFERENCES PersonGroup (idGroup),
-	CONSTRAINT Rate_PersonHasGroup_fk FOREIGN KEY (idPersonHasGroup) REFERENCES PersonHasGroup (idPersonHasGroup)
+  CONSTRAINT Rate_PersonGroup_fk FOREIGN KEY (idGroup) REFERENCES ṔPersonGroup (idGroup),
+	CONSTRAINT Rate_Person_fk FOREIGN KEY (idPerson) REFERENCES Person (idPerson),
+	CONSTRAINT Rate_Person_fk FOREIGN KEY (idPersonEvaluated) REFERENCES Person (idPerson)
 );
-
